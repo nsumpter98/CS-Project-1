12197e98f763d9ec6eb2ce87a44057cc5395522a
Nathanael Sumpter <nsumpter@winamaccoilspring.com>
2022-09-29 13:41:14 -0400

    added randomNameStartingWith function

--- src/prj1_src/Loader.java
+++ src/prj1_src/Loader.java
@@ -9,17 +9,29 @@ public class Loader {
 
 
         //create a new NamePopRanking object
-        NamePopRanking namePopRanking = new NamePopRanking(3);
+        NamePopRanking namePopRanking = new NamePopRanking(5);
         //create a new NameRandomStarting object
         NameRandomStarting nameRandomStarting = new NameRandomStarting();
         //create a new Baby object
         Baby baby = new Baby("John", 1, 100, "M");
         Baby baby2 = new Baby("Barb", 2, 200, "F");
+        Baby baby4 = new Baby("Beth", 2, 200, "F");
+        Baby baby5 = new Baby("Beet", 2, 200, "F");
         Baby baby3 = new Baby("John", 2, 200, "F");
         //add the baby to the NamePopRanking object
         namePopRanking.addBaby(baby);
         namePopRanking.addBaby(baby2);
         namePopRanking.addBaby(baby3);
+        namePopRanking.addBaby(baby4);
+        namePopRanking.addBaby(baby5);
+
+
+
+        namePopRanking.randomNameStartingWith();
+
+        namePopRanking.displayRankingStartingWith();
+
+
 
         //print out set of names from loadNames function
         System.out.println(namePopRanking.bothGendersName() + " " + namePopRanking.bothGendersName().size());
--- src/prj1_src/NamePopRanking.java
+++ src/prj1_src/NamePopRanking.java
@@ -2,6 +2,7 @@ package prj1_src;
 
 
 import java.util.HashSet;
+import java.util.Random;
 import java.util.Scanner;
 import java.util.Set;
 
@@ -52,7 +53,7 @@ public class NamePopRanking {
             System.out.println(babyArray[i].getName());
             System.out.println(babyArray[i].getGender());
             if (babyArray[i].getName().equals(name.trim())) {
-                if(babyArray[i].getGender().equals(gender.trim())) {
+                if (babyArray[i].getGender().equals(gender.trim())) {
                     //print the ranking of the name
                     System.out.println("Rank: " + babyArray[i].getRank());
                     //break out of the loop
@@ -65,50 +66,118 @@ public class NamePopRanking {
     }
 
 
-//function to load all the data from the babyArray into Set<String>
+    //function to load all the data from the babyArray into Set<String>
 //and return the set
-public Set<String> bothGendersName() {
-    //create a new set
-    Set<String> names = new HashSet<>();
-    Set<String> Mnames = new HashSet<>();
-    Set<String> Fnames = new HashSet<>();
-    //create a counter
-    int i = 0;
-    //while the counter is less than the size of the array
-    while (i < size) {
-        if(babyArray[i].getGender().equals("M")) {
-            //add the name to the set
-            Mnames.add(babyArray[i].getName());
-        }
-        else {
-            Fnames.add(babyArray[i].getName());
-        }
-        //add the name in the array to the set
+    public Set<String> bothGendersName() {
+        //create a new set
+        Set<String> names = new HashSet<>();
+        Set<String> Mnames = new HashSet<>();
+        Set<String> Fnames = new HashSet<>();
+        //create a counter
+        int i = 0;
+        //while the counter is less than the size of the array
+        while (i < size) {
+            if (babyArray[i].getGender().equals("M")) {
+                //add the name to the set
+                Mnames.add(babyArray[i].getName());
+            } else {
+                Fnames.add(babyArray[i].getName());
+            }
+            //add the name in the array to the set
 
-        //increment the counter
-        i++;
+            //increment the counter
+            i++;
+        }
+
+        for (int j = 0; j < size; j++) {
+            if (Mnames.contains(babyArray[j].getName()) && Fnames.contains(babyArray[j].getName())) {
+                //add the name to the set
+                names.add(babyArray[j].getName());
+            }
+
+        }
+
+
+        //return the set
+        return names;
     }
 
-    for (int j = 0; j < size; j++) {
-        if(Mnames.contains(babyArray[j].getName()) && Fnames.contains(babyArray[j].getName())) {
-            //add the name to the set
-            names.add(babyArray[j].getName());
+
+    //prompts the user to enter gender, and string and
+//display the names and their rankings that begin with the specific string.
+    public void displayRankingStartingWith() {
+        //create a new scanner object
+        Scanner scanner = new Scanner(System.in);
+        //prompt the user to enter gender
+        System.out.println("Enter gender: ");
+        //store the user input in a variable
+        String gender = scanner.next();
+        //prompt the user to enter a string
+        System.out.println("Enter a string: ");
+        //store the user input in a variable
+        String string = scanner.next();
+        //create a counter
+        int i = 0;
+        //while the counter is less than the size of the array
+        while (i < size) {
+            //if the name in the array starts with the string entered by the user
+            if (babyArray[i].getName().startsWith(string) && babyArray[i].getGender().equals(gender)) {
+                //print the name and the ranking
+                System.out.println("Name: " + babyArray[i].getName() + " Rank: " + babyArray[i].getRank());
+            }
+            //increment the counter
+            i++;
         }
 
     }
 
 
-    //return the set
-    return names;
-}
-
-
-
-
-
-
-
-
+    // Find a random name start with a letter or substring from user input
+    public void randomNameStartingWith() {
+        //create a new scanner object
+        Scanner scanner = new Scanner(System.in);
+        //prompt the user to enter gender
+        System.out.println("Enter gender: ");
+        //store the user input in a variable
+        String gender = scanner.next();
+        //prompt the user to enter a string
+        System.out.println("Enter a string: ");
+        //store the user input in a variable
+        String string = scanner.next();
+        //create a counter
+        int i = 0;
+        //create a new set
+        Set<String> names = new HashSet<>();
+        //while the counter is less than the size of the array
+        while (i < size) {
+            //if the name in the array starts with the string entered by the user
+            if (babyArray[i].getName().startsWith(string) && babyArray[i].getGender().equals(gender)) {
+                //add the name to the set
+                names.add(babyArray[i].getName());
+            }
+            //increment the counter
+            i++;
+        }
+        //create a new array
+        String[] nameArray = new String[names.size()];
+        //create a counter
+        int j = 0;
+        //for each name in the set
+        for (String name : names) {
+            //add the name to the array
+            nameArray[j] = name;
+            System.out.println(nameArray[j]);
+            //increment the counter
+            j++;
+        }
+        System.out.println(nameArray.toString());
+        //create a random number generator
+        Random rand = new Random();
+        //create a random number
+        int randNum = rand.nextInt(names.size() + 1);
+        //print the random name
+        System.out.println("Random name: " + nameArray[randNum]);
+    }
 
 
     public void printBabyArray() {
@@ -119,50 +188,7 @@ public Set<String> bothGendersName() {
         }
     }
 
-    public void sortBabyArray() {
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size - 1; j++) {
-                if (babyArray[j] != null && babyArray[j + 1] != null) {
-                    if (babyArray[j].getCount() < babyArray[j + 1].getCount()) {
-                        Baby temp = babyArray[j];
-                        babyArray[j] = babyArray[j + 1];
-                        babyArray[j + 1] = temp;
-                    }
-                }
-            }
-        }
-    }
 
-    public void printTopTen() {
-        for (int i = 0; i < 10; i++) {
-            if (babyArray[i] != null) {
-                System.out.println(babyArray[i].toString());
-            }
-        }
-    }
-
-    public void printBottomTen() {
-        for (int i = size - 1; i > size - 11; i--) {
-            if (babyArray[i] != null) {
-                System.out.println(babyArray[i].toString());
-            }
-        }
-    }
-
-    public void printTopTenGirls() {
-        int count = 0;
-        for (int i = 0; i < size; i++) {
-            if (babyArray[i] != null) {
-                if (babyArray[i].getGender().equals("F")) {
-                    System.out.println(babyArray[i].toString());
-                    count++;
-                }
-            }
-            if (count == 10) {
-                break;
-            }
-        }
-    }
 
 }
 
